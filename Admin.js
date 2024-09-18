394ad416c627ec7341210ec109750f9f55ebe542
Tim Dobson <hello@tdobson.net>
2024-09-18 14:43:08 +0100

    fix: make last_name explicit in Admin.js

--- Admin.js
+++ Admin.js
@@ -5,7 +5,7 @@ function readAdmin(){
 let cell = setupCell("Dashboard","B5")
 let sheet = setupSheet("Admin")
  
- var results = stmt.executeQuery('select distinct order_id, db.`first_name`,db.`last_name`,`nickname` fbname, `admin-phone-number`, `admin-participation-statement-one`, `admin-participation-statement-two`   from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by db.`first_name` ASC');
+ var results = stmt.executeQuery('select distinct order_id, db.`first_name`, db.`last_name`, `nickname` fbname, `admin-phone-number`, `admin-participation-statement-one`, `admin-participation-statement-two`   from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by db.`first_name` ASC');
 
 appendToSheet(sheet, results);
 results.close();
